adb093a18005df89d7a5cc7cd600bdd16a36a087
Richard Chesterwood <richard@inceptiontraining.co.uk>
2016-09-27 20:52:36 +0100

    beginning work on Netflix OSS integration

--- src/main/java/com/virtualpairprogrammers/controllers/VehicleController.java
+++ src/main/java/com/virtualpairprogrammers/controllers/VehicleController.java
@@ -1,10 +1,13 @@
 package com.virtualpairprogrammers.controllers;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,48 +26,56 @@ public class VehicleController
 	@Autowired
 	private VehicleRepository data;
 
+	@Autowired
+	private LoadBalancerClient balancer;
+
 	@RequestMapping(value="/newVehicle.html",method=RequestMethod.POST)
 	public String newVehicle(Vehicle vehicle)
 	{
 		data.save(vehicle);
 		return "redirect:/website/vehicles/list.html";
 	}
-	
+
 	@RequestMapping(value="/deleteVehicle.html", method=RequestMethod.POST)
 	public String deleteVehicle(@RequestParam Long id)
 	{
 		data.delete(id);
 		return "redirect:/website/vehicles/list.html";		
 	}
-	
+
 	@RequestMapping(value="/newVehicle.html",method=RequestMethod.GET)
 	public ModelAndView renderNewVehicleForm()
 	{
 		Vehicle newVehicle = new Vehicle();
 		return new ModelAndView("newVehicle","form",newVehicle);
 	} 
-	
+
 	@RequestMapping(value="/list.html", method=RequestMethod.GET)	
 	public ModelAndView vehicles()
 	{
 		List<Vehicle> allVehicles = data.findAll();
 		return new ModelAndView("allVehicles", "vehicles", allVehicles);
 	}
-	  
+
 	@RequestMapping(value="/vehicle/{name}")
 	public ModelAndView showVehicleByName(@PathVariable("name") String name)
 	{
 		Vehicle vehicle = data.findByName(name);
+
+		ServiceInstance service = balancer.choose("fleetman-position-tracker");
 		
+		// if none available, null pointer.
+		URI uri = service.getUri();
 		// get the current position for this vehicle from the microservice
-		// TODO - lots to do here to get rid of this horrid hardcoding!!!
+
+		// TODO upgrade this - it's better but still....
 		RestTemplate rest = new RestTemplate();
-		Position response = rest.getForObject("http://localhost:8090/vehicles/" + name, Position.class);
-		
+		Position response = rest.getForObject(uri.toString() + "/vehicles/" + name, Position.class);
+
 		Map<String,Object> model = new HashMap<>();
 		model.put("vehicle", vehicle);
 		model.put("position", response);
 		return new ModelAndView("vehicleInfo", "model",model);
 	}
-	
+
 }
