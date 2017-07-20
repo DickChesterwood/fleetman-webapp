c91fd64602d637bbea0e75fc941fa2ddb981481f
Dick Chesterwood <richard@inceptiontraining.co.uk>
2017-07-20 14:56:58 +0100

    upgrades to front end. Still work to be done on following vehicles

--- src/main/java/com/virtualpairprogrammers/services/RemoteMicroserviceStubImpl.java
+++ src/main/java/com/virtualpairprogrammers/services/RemoteMicroserviceStubImpl.java
@@ -12,19 +12,21 @@ import com.virtualpairprogrammers.controllers.Position;
 public class RemoteMicroserviceStubImpl implements RemotePositionMicroserviceCalls 
 {	
 	// This stub implementation simply returns a random position in a small area.
-	// Something in the area of 53.3xxxx and -1.6xxxxxx
+	// Something in the area of 53.38207,-1.48423
 	public Position getLatestPositionForVehicle(String name) 
-	{
-		String randomFraction = "" + Math.random();
-		randomFraction = randomFraction.substring(2, randomFraction.length());
-				
-        BigDecimal lat = new BigDecimal("53.35" + randomFraction);
-
-		randomFraction = "" + Math.random();
-		randomFraction = randomFraction.substring(2, randomFraction.length());
-		
-		BigDecimal longitude = new BigDecimal("-1.54" + randomFraction);
+	{	
+        BigDecimal lat = generateRandomBigDecimalFromRange("53.38653","53.37687" );		
+		BigDecimal longitude = generateRandomBigDecimalFromRange("-1.49850","-1.46517");
 		return new Position(lat, longitude,new java.util.Date(), true, name);
+	}
+	
+	public static BigDecimal generateRandomBigDecimalFromRange(BigDecimal min, BigDecimal max) {
+	    BigDecimal randomBigDecimal = min.add(new BigDecimal(Math.random()).multiply(max.subtract(min)));
+	    return randomBigDecimal.setScale(6,BigDecimal.ROUND_HALF_UP);
+	}
+
+	public static BigDecimal generateRandomBigDecimalFromRange(String min, String max) {
+	    return generateRandomBigDecimalFromRange(new BigDecimal(min), new BigDecimal(max));
 	}
 
 }
